4515945752c2bbe6ce6f0f4940c1627e39bc1c2e
spencerwilson123321 <spencerwilson123321@gmail.com>
2021-12-02 17:20:21 -0800

    fixed packet module

--- packet.py
+++ packet.py
@@ -10,11 +10,11 @@ class PacketType(Enum):
 
 class Packet:
 
-    def __init__(self, pkt_type, seq_num, dst_ip, dst_port):
+    def __init__(self, pkt_type, seq_num, dst_addr):
         self.pkt_type = pkt_type
         self.seq_num = seq_num
         # dst_addr = (ip, port) of the destination
-        self.dst_addr = (dst_ip, dst_port)
+        self.dst_addr = dst_addr
         if pkt_type == PacketType.DATA:
             self.data = '0123456789'
         if pkt_type == PacketType.ACK:
