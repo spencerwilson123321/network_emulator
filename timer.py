9ea7a05ef0aadd37fc26b80cd3e4707cd12df4a0
spencerwilson123321 <spencerwilson123321@gmail.com>
2023-06-01 22:58:51 -0700

    Removing bad docstrings.

--- timer.py
+++ timer.py
@@ -1,27 +1,23 @@
-# timer.py
 import time
 
-"""
-General purpose timer class that starts and stops a stopwatch,
-and can return the elapsed time using check_time.
-"""
+
 class Timer:
+    """
+        General purpose timer class that starts and stops a stopwatch,
+        and can return the elapsed time using check_time.
+    """
 
     def __init__(self):
         self._start_time = None
 
-    """ Start timer. """
     def start(self):
         self._start_time = time.perf_counter()
 
-    """ Sets the timer to 0, and returns the elapsed time (ms). """
     def stop(self):
-        """Stop the timer, and report the elapsed time"""
         elapsed_time = time.perf_counter() - self._start_time
         self._start_time = None
         return float(format(elapsed_time*1000, ".2f"))
 
-    """ returns the elapsed time (ms) since start, but doesn't stop the timer. """
     def check_time(self):
         elapsed_time = time.perf_counter() - self._start_time
         return float(format(elapsed_time*1000, ".2f"))
