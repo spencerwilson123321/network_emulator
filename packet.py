a67abb067acd0fdfd3ed2ccd6277501167a2e09d
spencerwilson123321 <spencerwilson123321@gmail.com>
2023-06-04 12:54:49 -0700

    Decided to use the struct module since it makes creating the packets much much easier

--- packet.py
+++ packet.py
@@ -1,3 +1,4 @@
+from struct import pack, unpack
 
 # Packet:
 # type: offset 0 (First Byte) ACK, DATA, EOT
@@ -6,14 +7,17 @@
 # data: length bytes starting at offset 3.
 
 def get_packet_type(pkt: bytes):
-    return int(pkt[0:1])
+    return pkt[0]
 
 def get_packet_number(pkt: bytes):
-    return int(pkt[1:2])
+    return pkt[1]
 
 def get_packet_data_length(pkt: bytes):
-    return int(pkt[2:3])
+    return pkt[2]
 
 def get_packet_data(pkt: bytes):
-    len = int(pkt[2:3])
+    len = pkt[2]
     return pkt[3:3+len]
+
+def create_packet(pkt_type: int, number: int, length: int, data: bytes) -> bytes:
+    return pack("BBB", pkt_type, number, length) + data
