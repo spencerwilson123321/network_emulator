b104ca55764e326b35bd96bbbd5ea2095971dc58
spencerwilson123321 <spencerwilson123321@gmail.com>
2023-06-05 14:15:07 -0700

    Completely rewriting the network simulator using raw sockets and asyncio so that it can be used with both TCP and UDP traffic using IPv4 addresses.

--- network.py
+++ network.py
@@ -1,100 +1,93 @@
+import asyncio
+import select 
 import sys
 import traceback
-import pickle
 import socket
 import threading
 import random
 import time
 import logging
 import configparser
-from packet import Packet, PacketType
+import struct
 
-logging.basicConfig(filename='network.log',
-                    encoding='utf-8',
-                    level=logging.INFO,
-                    format="%(asctime)s - %(message)s")
+BUFFSIZE = 65535
 
 class NetworkSimulator:
-    """
-        The NetworkSimulator class defines all the properties and behaviours of the simulated network.
-        It drops packets to achieve a specified bit error rate. It also waits for the given average delay before
-        sending packets to their destination in order to simulate propagation time and queueing/processing delay.
-    """
 
     def __init__(self, configuration):
+        
         self.receiver_address = (configuration["receiver"]["ip"], int(configuration["receiver"]["port"]))
         self.sender_address = (configuration["sender"]["ip"], int(configuration["sender"]["port"]))
         self.network_address = (configuration["network"]["ip"], int(configuration["network"]["port"]))
-        # The bit error rate, rate that packets are dropped.
+
+        # Packet loss rate
         self.loss_rate = configuration["network"]["loss_rate"]
-        # Average delay each packet must wait before being sent to its destination.
-        self.avg_delay = configuration["network"]["delay"]
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.socket.bind((nIP, nPort_send))
+        # Delay each packet must wait before being sent to its destination.
+        self.delay = configuration["network"]["delay"]
+
+        # Using raw sockets so that all types of traffic will be captured.
+        self.socket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
+        self.socket.bind(("lo", 0x0800))
         self.socket.setblocking(0)
-        self.lock = threading.Lock()
+
+        self.poller = select.epoll()
+        self.poller.register(self.socket.fileno())
         self.num_lost_packets = 0
-
-    def discard_pkt(self):
-        return random.random() < self.ber
-
-    def receive_packet(self):
-        data, addr = self.receiveSocket.recvfrom(1024)
-        return pickle.loads(data), addr
-
-    def send_packet_to(self, addr, packet):
-        time.sleep(self.avg_delay*(1/1000))
-        self.lock.acquire()
-        self.sendSocket.sendto(pickle.dumps(packet), addr)
-        self.lock.release()
-        logging.info(f"Sending: {packet}")
-
-    def increment_lost_packets(self):
-        self.num_lost_packets = self.num_lost_packets + 1
-
-
-def main():
-
-    CONFIG = configparser.ConfigParser()
-    CONFIG.read("config.ini")
-    simulator = NetworkSimulator(CONFIG)
-
-    # The main loop checks for incoming packets, for each packet that is received, it checks ber and
-    # potentially drops the packet. If the packet is not dropped then the packet is sent to a new thread which
-    # waits for the average delay and then sends the packet to it's destination.
     
-    while True:
-        try:
-            pkt, addr = simulator.receive_packet()
-            if pkt is None:
-                continue
-        except BlockingIOError:
-            continue
-        logging.info(f"Received: {pkt}")
-        if pkt.pkt_type == PacketType.EOT:
-            final_thread = threading.Thread(target=simulator.send_packet_to, args=(pkt.dst_addr, pkt,))
-            # Waiting for the final thread to finish before shutting down the main thread. This prevents
-            # the main thread from closing the final_thread before it has sent the last packet.
-            final_thread.start()
-            final_thread.join()
-            break
-        # Check BER and discard packet
-        if simulator.discard_pkt():
-            logging.info(f"Discarding: {pkt}")
-            simulator.increment_lost_packets()
-            continue
-        else:
-            # Start thread for each packet with avg_delay
-            threading.Thread(target=simulator.send_packet_to, args=(pkt.dst_addr, pkt,)).start()
-    logging.info(f"Total lost packets: {simulator.num_lost_packets}")
+    def cleanup(self):
+        print("\nShutting down network simulator...")
+        self.poller.unregister(self.socket.fileno())
+        self.socket.close()
+    
+    def is_recognized_address(self, ip, port):
+        return (ip == self.sender_address[0] and port == self.sender_address[1]) or (ip == self.receiver_address[0] and port == self.receiver_address[1]) 
+
+    async def run_until_done(self):
+        """
+        while True:
+            # 1. Poll socket for packets.
+            # 2. Read raw packet.
+            # 2. Set the source IP address to our IP address.
+            # 3. Set the destination IP address to the mapped IP address.
+            # 4. Simulate packet loss.
+            # 5. Asyncio - Simulate network delay.
+            # 6. Transmit packet to mapped destination address if no loss.
+        """
+        ETH_HDR_LEN = 14
+        SRC_IP_OFFSET = ETH_HDR_LEN + 12
+        DST_IP_OFFSET = SRC_IP_OFFSET + 4
+        SRC_PORT_OFFSET = DST_IP_OFFSET + 4
+        DST_PORT_OFFSET = SRC_IP_OFFSET + 2
+        while True:
+            events = self.poller.poll()
+            for fd, event in events:
+                if event & select.EPOLLIN:
+                    # Read raw packet.
+                    packet, address = self.socket.recvfrom(BUFFSIZE)
+                    # Get source and destination address.
+                    source_ip = socket.inet_ntoa(packet[SRC_IP_OFFSET:SRC_IP_OFFSET+4])
+                    destination_ip = socket.inet_ntoa(packet[DST_IP_OFFSET:DST_IP_OFFSET+4])
+                    source_port = struct.unpack(packet[SRC_PORT_OFFSET:SRC_PORT_OFFSET+2])
+                    destination_port = struct.unpack(packet[DST_PORT_OFFSET:DST_PORT_OFFSET+2])
+                    # If unrecognized IP address, then skip.
+                    if not self.is_recognized_address(source_ip, source_port):
+                        continue
+                    # TODO: Network Address Translation
 
 
 if __name__ == '__main__':
-
+    logging.basicConfig(filename='network.log',
+                    encoding='utf-8',
+                    level=logging.INFO,
+                    format="%(asctime)s - %(message)s")
     try:
-        main()
+        CONFIG = configparser.ConfigParser()
+        CONFIG.read("config.ini")
+        simulator = NetworkSimulator(CONFIG)
+        asyncio.run(simulator.run_until_done())
     except KeyboardInterrupt:
-        print("\nShutting down network...")
+        simulator.cleanup()
     except Exception:
+        simulator.cleanup()
         traceback.print_exc(file=sys.stdout)
     sys.exit(-1)
